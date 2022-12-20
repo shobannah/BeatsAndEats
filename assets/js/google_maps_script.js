c5095d13c0e79fed573882082137afc827e682bd
Carlos Torres Orona <ce.torresorona@gmail.com>
2022-12-19 20:39:21 -0500

    Augmeneted Google Maps API function to show nearby searches

--- assets/js/google_maps_script.js
+++ assets/js/google_maps_script.js
@@ -48,10 +48,25 @@
 let map, infoWindow;
 
 function initMap() {
+  const orlando = { lat: 28.5384, lng: -81.3789 };
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 28.5384, lng: -81.3789 },
+    center: orlando,
     zoom: 10,
   });
+
+   // Create the places service.
+   const service = new google.maps.places.PlacesService(map);
+   let getNextPage;
+   const moreButton = document.getElementById("more");
+ 
+   moreButton.onclick = function () {
+     moreButton.disabled = true;
+     if (getNextPage) {
+       getNextPage();
+     }
+   };
+
+  // Add info Window
   infoWindow = new google.maps.InfoWindow();
 
   const locationButton = document.createElement("button");
@@ -83,7 +98,55 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+
+ // Perform a nearby search.
+ service.nearbySearch(
+  { location: orlando, radius: 500, type: "store" },
+  (results, status, pagination) => {
+    if (status !== "OK" || !results) return;
+
+    addPlaces(results, map);
+    moreButton.disabled = !pagination || !pagination.hasNextPage;
+    if (pagination && pagination.hasNextPage) {
+      getNextPage = () => {
+        // Note: nextPage will call the same handler function as the initial call
+        pagination.nextPage();
+      };
+    }
+  }
+);
 }
+
+function addPlaces(places, map) {
+const placesList = document.getElementById("places");
+
+for (const place of places) {
+  if (place.geometry && place.geometry.location) {
+    const image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25),
+    };
+
+    new google.maps.Marker({
+      map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location,
+    });
+
+    const li = document.createElement("li");
+
+    li.textContent = place.name;
+    placesList.appendChild(li);
+    li.addEventListener("click", () => {
+      map.setCenter(place.geometry.location);
+    });
+  }
+  }
+};
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
