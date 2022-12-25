e20415c6f7bcb182ef4ecf2284be00c4199a254d
Carlos Torres Orona <ce.torresorona@gmail.com>
2022-12-24 22:08:12 -0500

    Accomplished centering map based on geolocation and query places to study

--- assets/js/google_maps_script.js
+++ assets/js/google_maps_script.js
@@ -1,77 +1,77 @@
 // Google Maps API Basic Construct
 
-let map, infoWindow;
+var map;
+var service;
+var infoWindow;
+var currentPlace;
 
-function initMap() {
-  const orlando = { lat: 28.5384, lng: -81.3789 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: orlando,
-    zoom: 10,
+function initialize() {
+
+  currentPlace = { lat: 28.5384, lng: -81.3789 };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: currentPlace,
+    zoom: 15
   });
 
-  // Create the places service.
-  const service = new google.maps.places.PlacesService(map);
-  let getNextPage;
-  const moreButton = document.getElementById("more");
-
-  moreButton.onclick = function () {
-    moreButton.disabled = true;
-    if (getNextPage) {
-      getNextPage();
-    }
-  };
-
-  // Add info Window
   infoWindow = new google.maps.InfoWindow();
 
-  // Perform a nearby search.
-  service.nearbySearch(
-    { location: orlando, radius: 500, type: "store" },
-    (results, status, pagination) => {
-      if (status !== "OK" || !results) return;
+  function getCurrentLocation() {
+    console.log("Getting current location");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-      addPlaces(results, map);
-      moreButton.disabled = !pagination || !pagination.hasNextPage;
-      if (pagination && pagination.hasNextPage) {
-        getNextPage = () => {
-          // Note: nextPage will call the same handler function as the initial call
-          pagination.nextPage();
-        };
-      }
-    }
-  );
-}
-
-function addPlaces(places, map) {
-  const placesList = document.getElementById("places");
-
-  for (const place of places) {
-    if (place.geometry && place.geometry.location) {
-      const image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      new google.maps.Marker({
-        map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location,
-      });
-
-      const li = document.createElement("li");
-
-      li.textContent = place.name;
-      placesList.appendChild(li);
-      li.addEventListener("click", () => {
-        map.setCenter(place.geometry.location);
-      });
+          currentPlace = pos;
+          console.log(currentPlace);
+          map.setCenter(pos);
+          searchStudyPlaces();
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+      searchStudyPlaces();
     }
   }
+
+  getCurrentLocation();
+
+  function searchStudyPlaces() {
+    console.log(currentPlace);
+    var request = {
+      location: currentPlace,
+      radius: '1200',
+      query: 'places to study'
+    };
+
+    function createMarker(place) {
+      console.log(place.name);
+      new google.maps.Marker({
+        tile: place.name,
+        position: place.geometry.location,
+        map: map
+      });
+    }
+
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(results[i]);
+        }
+      }
+    }
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+  }
+
 };
-
-
-window.initMap = initMap;
